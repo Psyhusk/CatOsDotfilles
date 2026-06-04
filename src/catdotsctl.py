0b79b454178bf6897b9bc83d3fbcd18e97a7f664
Psyhusk <luizmarcosabrilsilvaoutlock@gmail.com>
2026-06-04 12:31:32 -0300

    Add files via upload

---
+++ src/catdotsctl.py
@@ -0,0 +1,9 @@
+import typer
+
+from catdots.ctl.services.waybar import cli_api as waybar
+
+# Main typer app
+cli = typer.Typer()
+
+# Commands, every cli_api returns a typer app containing its subcommands
+cli.add_typer(waybar(), name="waybar")
