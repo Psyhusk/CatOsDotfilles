bd52979ce8a360eb78c55eb91c93d2ff70f4e360
Psyhusk <luizmarcosabrilsilvaoutlock@gmail.com>
2026-06-04 12:33:31 -0300

    Add files via upload

---
+++ src/actions/get_hypridle_status.py
@@ -0,0 +1,28 @@
+# import logging
+
+# from catdots.library.logger import setup_actions_state
+from catdots.library.procs_utils import is_running
+from ._base import BaseAction
+
+
+# setup_actions_state(__name__)
+# logger = logging.getLogger(__name__)
+
+
+class GetHypridleStatus(BaseAction):
+    def main(self) -> None:
+        if is_running("hypridle"):
+            data = {
+                "text": "On",
+                "class": "active",
+                "tooltip": "Screen locking active\nLeft: Deactivate\nRight: Lock Screen \nScreen will be locked after 5 minutes of inactivity.",
+            }
+
+        else:
+            data = {
+                "text": "Off",
+                "class": "notactive",
+                "tooltip": "Screen locking deactivated\nLeft: Activate\nRight: Lock Screen",
+            }
+
+        self.send(data)
